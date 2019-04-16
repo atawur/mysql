27f2bddc2aeb5213fd5a98c1fb36a02e1c72ecc9
Atik <rahmanatawur@gmail.com>
2019-04-17 02:46:11 +0600

    db updated

---
+++ students_management_leftJoin.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 16, 2019 at 10:45 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.2.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `students_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `name` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`id`, `name`) VALUES
+(1, 'Bangla'),
+(2, 'English');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students_course`
+--
+
+CREATE TABLE `students_course` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `students_course`
+--
+
+INSERT INTO `students_course` (`id`, `student_id`, `course_id`) VALUES
+(1, 20, 1),
+(2, 20, 2),
+(5, 28, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_information`
+--
+
+CREATE TABLE `student_information` (
+  `id` int(11) NOT NULL,
+  `student_name` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `student_information`
+--
+
+INSERT INTO `student_information` (`id`, `student_name`, `description`, `email`) VALUES
+(20, 'atik', '', 'abc@gmail.com'),
+(28, 'Mr Nazmul', '', 'rahmanatddawu111r@gmail.com'),
+(29, 'mr kazal', '', 'test@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasklist`
+--
+
+CREATE TABLE `tasklist` (
+  `task_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `start_date` date DEFAULT NULL,
+  `due_date` date DEFAULT NULL,
+  `status` tinyint(4) NOT NULL,
+  `priority` tinyint(4) NOT NULL,
+  `descriptions` varchar(50) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `students_course`
+--
+ALTER TABLE `students_course`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_student` (`student_id`);
+
+--
+-- Indexes for table `student_information`
+--
+ALTER TABLE `student_information`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `tasklist`
+--
+ALTER TABLE `tasklist`
+  ADD PRIMARY KEY (`task_id`);
+ALTER TABLE `tasklist` ADD FULLTEXT KEY `descriptions` (`descriptions`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `students_course`
+--
+ALTER TABLE `students_course`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `student_information`
+--
+ALTER TABLE `student_information`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `tasklist`
+--
+ALTER TABLE `tasklist`
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `students_course`
+--
+ALTER TABLE `students_course`
+  ADD CONSTRAINT `fk_student` FOREIGN KEY (`student_id`) REFERENCES `student_information` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
